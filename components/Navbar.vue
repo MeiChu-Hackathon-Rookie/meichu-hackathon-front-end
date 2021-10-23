9b3d48d26bd86dea0238888d3ab0e5e564138e9e
MOUSSO <jackysoon871008@gmail.com>
2021-10-23 16:53:52 +0800

    feat: Add navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="navbar">
+    <div class="nav-brand">
+      <img src="" alt="">
+      <div class="h2 gray">E-Learning tool</div>
+    </div>
+    <ul class="nav-item-list">
+      <nuxt-link to="/">
+        <li class="nav-item h3 gray">
+          <i class="el-icon-s-home"></i>
+          Overview
+        </li>
+      </nuxt-link>
+      <nuxt-link to="/VideoPlayer">
+        <li class="nav-item h3 gray">
+          <i class="el-icon-video-play"></i>
+          Video Player
+        </li>
+      </nuxt-link>
+      <nuxt-link to="/Upload">
+        <li class="nav-item h3 gray">  
+          <i class="el-icon-upload"></i>
+          Upload
+        </li>
+      </nuxt-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+#navbar {
+  background-color: $dark-blue;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 255px;
+  // padding: 10px 20px;
+}
+
+.nav-brand {
+  text-align: center;
+  h1 {
+    color: $light-blue;
+  }
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+
+  a {
+    color: $light-blue;
+    text-decoration: none;
+
+    li {
+      // list-style-type: none;
+      transition: 0.5s;
+      padding: 1em 0 1em 32px;
+      &:hover {
+        background-color: #9FA2B4;
+        color: $dark-blue;
+      }
+
+      i {
+        margin-right: 24px;
+      }
+    }
+  }
+}
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     'element-ui/lib/theme-chalk/index.css',
-    '@assets/styles/global.scss',
+    '@/assets/styles/global.scss',
+    '@/assets/styles/font.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
